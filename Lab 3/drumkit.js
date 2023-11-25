08d6565d12e377bcdf6234966e4ae6f511bb1831
Kamila Marcinek <kamila.weronika.marcinek@gmail.com>
2023-11-25 16:41:16 +0100

    rename

--- Lab 3/drumkit.js
+++ Lab 3/drumkit.js
@@ -1,67 +1,101 @@
-const recordedChannels = [[], [], [], []];
-let recordingChannel = 0;
-let recordingStartTime = null;
-const MAX_RECORDING_DURATION = 10 * 1000;
-document.addEventListener('keypress', onKeyPress)
-
 const KeyToSound = {
-    'a': document.querySelector('#s1'),
-    's': document.querySelector('#s2'),
-    'd': document.querySelector('#s3'),
-    'f': document.querySelector('#s4'),
-    'g': document.querySelector('#s5'),
-    'h': document.querySelector('#s6'),
-    'j': document.querySelector('#s7'),
-    'k': document.querySelector('#s8'),
-    'l': document.querySelector('#s9'),
+  a: document.getElementById('clap'),
+  s: document.getElementById('hithat'),
+  d: document.getElementById('kick'),
+  f: document.getElementById('openhat'),
+  g: document.getElementById('boom'),
+  h: document.getElementById('ride'),
+  j: document.getElementById('snare'),
+  k: document.getElementById('tom'),
+  l: document.getElementById('tink')
 }
 
-function onKeyPress(event) {
-    const sound = KeyToSound[event.key]
-    playSound(sound)
-    recordSound(sound)
-}
-function playSound(sound) {
-    if (sound) {
-        sound.currentTime = 0;
-        sound.play();
+document.addEventListener('keydown', onKeyPress)
+document.addEventListener('keyup', onKeyRelease)
+
+function onKeyPress (event) {
+  const sound = KeyToSound[event.key]
+  if (sound) {
+    sound.currentTime = 0
+    sound.play()
+    sound.parentElement.classList.add('pressed')
+    if (recordingChannel !== undefined) {
+      const time = Date.now() - channelStartTimes[recordingChannel - 1]
+      const soundData = {
+        sound: event.key,
+        time: time
+      }
+      channels[recordingChannel - 1].push(soundData)
     }
+  }
 }
 
-function recordSound(sound) {
-    const recordedChannel = recordedChannels[recordingChannel];
-    recordedChannel.push({ sound: sound.cloneNode(), timestamp: Date.now() });
-
-    if (!recordingStartTime) {
-        recordingStartTime = Date.now();
-        setTimeout(stopRecording, MAX_RECORDING_DURATION);
-    }
+function onKeyRelease (event) {
+  const sound = KeyToSound[event.key]
+  if (sound) {
+    sound.parentElement.classList.remove('pressed')
+  }
 }
 
-function stopRecording() {
-    recordingStartTime = null;
+let channelStartTimes = [0, 0, 0, 0]
+let recordingChannel
+
+const channels = [
+  [], // Channel 1
+  [], // Channel 2
+  [], // Channel 3
+  [] // Channel 4
+]
+
+function recordChannel (channelNumber) {
+  //if there is existing records, clean channel
+  if (channels[channelNumber - 1].length > 0) {
+    channels[channelNumber - 1] = []
+  }
+  const playElementId = `channel${channelNumber}Play`
+  document.getElementById(playElementId).classList.add('hidden')
+  const recElementId = `channel${channelNumber}Rec`
+  const icon = document.getElementById(recElementId).querySelector('.icon')
+  icon.classList.toggle('blink')
+  setRecordingParams(channelNumber)
+  setTimeout(() => {
+    stopRecording(channelNumber)
+  }, 5000)
 }
 
-function switchRecordingChannel(channelIndex) {
-    recordingChannel = channelIndex;
-    recordingStartTime = null;
+function setRecordingParams (channelNumber) {
+  console.log(channelNumber)
+  if (channelNumber >= 1 && channelNumber <= 4) {
+    recordingChannel = channelNumber
+    channelStartTimes[channelNumber - 1] = Date.now()
+  } else {
+    console.error('Invalid channel number')
+  }
 }
 
-function playRecordedChannel(channelIndex) {
-    const channel = recordedChannels[channelIndex];
-    for (const { sound, timestamp } of channel) {
-        const delay = Date.now() - timestamp;
-        setTimeout(() => {
-            sound.currentTime = 0;
-            sound.play();
-        }, delay);
-    }
+function stopRecording (channelNumber) {
+  recordingChannel = undefined
+  const playElementId = `channel${channelNumber}Play`
+  document.getElementById(playElementId).classList.remove('hidden')
+  const recElementId = `channel${channelNumber}Rec`
+  const icon = document.getElementById(recElementId).querySelector('.icon')
+  icon.classList.toggle('blink')
+}
+function playChannel (channelNumber) {
+  channels[channelNumber - 1].forEach(sound => {
+    setTimeout(() => {
+      playSound(KeyToSound[sound.sound])
+    }, sound.time)
+  })
+}
+function playSound (sound) {
+  sound.currentTime = 0
+  sound.play()
 }
 
-function playAllRecordedChannels() {
-    for (let i = 0; i < recordedChannels.length; i++) {
-        if (recordedChannels[i].length > 0) {
-            playRecordedChannel(i);
-        }
-    }
+function playAllChannels () {
+  if (channels[0].length > 0) playChannel(1)
+  if (channels[1].length > 0) playChannel(2)
+  if (channels[2].length > 0) playChannel(3)
+  if (channels[3].length > 0) playChannel(4)
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # WSEI-Programowanie-JS
+Projekty live można zobaczyć tutaj:
+https://karmelova.github.io/WSEI-Programowanie-JS/
